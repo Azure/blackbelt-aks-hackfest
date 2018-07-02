fa0833f83c493727dfae9f3160cd50bd08a57fbb
desreela <40350122+desreela@users.noreply.github.com>
2018-07-03 00:02:52 +0530

    Update brigade.js

--- labs/helper-files/brigade.js
+++ labs/helper-files/brigade.js
@@ -60,7 +60,7 @@ function kubeJobRunner (config, k) {
     k.storage.enabled = false
     k.image = "lachlanevenson/k8s-kubectl:v1.8.2"
     k.tasks = [
-        `kubectl set image deployment/heroes-web-deploy heroes-web-cntnr=<youracrhere>.azurecr.io/azureworkshop/rating-web:${config.get("imageTag")}`
+        `kubectl set image deployment/heroes-web-deploy heroes-web-cntnr=deepuacr01.azurecr.io/azureworkshop/rating-web:${config.get("imageTag")}`
     ]
 }
 
