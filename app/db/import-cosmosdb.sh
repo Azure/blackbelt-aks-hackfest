88bce1d235aa3109bf8ed2b231e01da2e257cc15
Jason De Lorme <jasondel@microsoft.com>
2019-05-02 08:37:48 -0400

    Removing initial load of ratings.

--- app/db/import-cosmosdb.sh
+++ app/db/import-cosmosdb.sh
@@ -1,5 +1,6 @@
 echo "Importing data into $MONGODB_URI ..."
 mongoimport --uri $MONGODB_URI --collection heroes --type json --file ./heroes.json --jsonArray
-mongoimport --uri $MONGODB_URI --collection ratings --type json --file ./ratings.json --jsonArray
+# Removing sample ratings.
+#mongoimport --uri $MONGODB_URI --collection ratings --type json --file ./ratings.json --jsonArray
 mongoimport --uri $MONGODB_URI --collection sites --type json --file ./sites.json --jsonArray
 echo "Completed import."
